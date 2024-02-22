599255cd1386b1129371956fa438295510a942ae
김용현 <yhkimjjang@gmail.com>
2024-02-22 12:14:44 +0900

    feat: KioskMainWindow 연결

--- WpfKiosk.Support/Local/ViewModels/BaseWindowViewModel.cs
+++ WpfKiosk.Support/Local/ViewModels/BaseWindowViewModel.cs
@@ -6,6 +6,7 @@ namespace WpfKiosk.Support.Local.ViewModels;
 
 public class BaseWindowViewModel
 {
+    // Close 이벤트 등록
     public ICommand CloseWindowCommand { get; init; }
 
     public BaseWindowViewModel()
@@ -13,6 +14,9 @@ public class BaseWindowViewModel
         CloseWindowCommand = new RelayCommand(CloseWindow);
     }
 
+    /// <summary>
+    /// Close 버튼을 클릭 했을 때 원도우 종료 수행
+    /// </summary>
     private void CloseWindow()
     {
         var rsult = MessageBox.Show("종료 하겠습니까?", "Close", MessageBoxButton.YesNo);
--- WpfKiosk/App.cs
+++ WpfKiosk/App.cs
@@ -1,6 +1,7 @@
 ﻿using Prism.Ioc;
 using Prism.Unity;
 using System.Windows;
+using WpfKiosk.Forms.UI.Views;
 using WpfKiosk.Support.UI.Units;
 
 namespace WpfKiosk;
@@ -9,7 +10,7 @@ internal class App : PrismApplication
 {
     protected override Window CreateShell()
     {
-        return new BaseWindow();
+        return new KioskMainWindow();
     }
 
     protected override void RegisterTypes(IContainerRegistry containerRegistry)
